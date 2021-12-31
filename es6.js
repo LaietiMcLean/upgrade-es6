ba436aef0f466c049ac704bff829c2467dd87eec
LaietiMcLean <laia.marco@bootcamp-upgrade.com>
2021-12-31 15:20:03 +0100

    Iteration#1

---
+++ es6.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+
+//Iteración 1
+let sum = (a = 10, b = 5) => {
+    console.log(a + b);
+}
+
+//Iteración 1.1 
+sum();
+
+//Iteración 1.2 
+sum(3);
+
+//Iteración 1.3
+sum(11, 17);
+
+}
